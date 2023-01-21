97f49e1f2746b7acf3f43f8bd821947e886b9a47
Jonathan Nuñez <37056901+JonathanNunezRdz@users.noreply.github.com>
2023-01-21 17:09:24 -0600

    prisma setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { ResultModule } from './result/result.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		PrismaModule,
+		UserModule,
+		ResultModule,
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
