0f628e1551b0a8d764552697e1bfeb6dca2eadff
Jonathan Nuñez <37056901+JonathanNunezRdz@users.noreply.github.com>
2023-01-24 18:49:11 +0000

    standarize score types

--- src/analysis/analysis.service.ts
+++ src/analysis/analysis.service.ts
@@ -13,15 +13,44 @@ export type Metrics = {
 	stdLettersPerWord: number;
 };
 
+export type FernandezHuertaScore = Score & {
+	difficulty: string
+	schoolGrade: string;
+};
+
+export type Score = {
+	score: number;
+};
+
 @Injectable()
 export class AnalysisService {
 	// main services
 
 	textAnalyzer(): Metrics {
+		const FHScore = this.alFH({} as Metrics);
+		FHScore.difficulty
 		return {} as Metrics;
 	}
 
 	// algorithm services
 
+	alFH(metrics: Metrics): FernandezHuertaScore {
+		// const {  } = metrics;
+		// matematicas
+		const scoreNum = 23;
+
+		return {
+			score: 23,
+			...this.getFHDifficulty(scoreNum),
+		};
+	}
+
 	// helper services
+
+	getFHDifficulty(score: number): Pick<FernandezHuertaScore, 'difficulty' | 'schoolGrade'> {
+		if (score < 30) return { difficulty: 'asd', schoolGrade: '' };
+		return { difficulty: 'asd', schoolGrade: '' };
+	}
+
+	getFHSchoolGrade() { }
 }
