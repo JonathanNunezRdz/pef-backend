8c9139200226762397978b8a2b4bb4f160a6b586
Francisco Bermudez <francisco.bermudez@udem.edu>
2023-01-31 18:49:18 -0600

    Algorithms Update
    mu work in progress

--- src/analysis/analysis.service.ts
+++ src/analysis/analysis.service.ts
@@ -15,7 +15,25 @@ export type Metrics = {
 
 export type FernandezHuertaScore = Score & {
 	difficulty: string
-	schoolGrade: string;
+	schoolGrade: string
+};
+
+export type GutierrezPoliniScore = Score & {
+	difficulty: string	
+};
+
+export type SzigrisztPazosScore = Score & {
+	difficulty: string
+	type: string
+	schoolGrade: string	
+};
+
+export type InfleszScore = Score & {
+	difficulty: string	
+};
+
+export type muScore = Score & {
+	difficulty: string	
 };
 
 export type Score = {
@@ -44,6 +62,55 @@ export class AnalysisService {
 		};
 	}
 
+	alGP(metrics: Metrics): GutierrezPoliniScore {
+		const { numOfLetters, numOfWords, numOfSentences } = metrics;
+		let scoreNum = 95.2 - 9.7 * (numOfLetters / numOfWords) - 0.35 * (numOfWords / numOfSentences);
+        scoreNum = Number(scoreNum.toFixed(2));				
+		return {
+			score: scoreNum,
+			...this.getGPDifficulty(scoreNum),
+		};
+	}
+
+	alCrawford(metrics: Metrics): Score {
+		const { numOfSyllables, numOfWords, numOfSentences } = metrics;
+		let SyPHW = 100 * numOfSyllables / numOfWords;
+		let SePHW = 100 * numOfSentences / numOfWords;
+		let scoreNum = -0.205 * SePHW + 0.049 * SyPHW - 3.407;
+        scoreNum = Number(scoreNum.toFixed(1));				
+		return {
+			score: scoreNum			
+		};
+	}
+
+	alSP(metrics: Metrics): SzigrisztPazosScore {
+		const { numOfSyllables, numOfWords, numOfSentences } = metrics;
+		let scoreNum = 206.835 - 62.3 * ( numOfSyllables / numOfWords) - (numOfWords / numOfSentences);
+        scoreNum = Number(scoreNum.toFixed(2));				
+		return {
+			score: scoreNum,
+			...this.getSPDifficulty(scoreNum),
+		};
+	}
+
+	alInflesz(metrics: Metrics): InfleszScore {
+		const { numOfSyllables, numOfWords, numOfSentences } = metrics;
+		let scoreNum = 206.835 - 62.3 * ( numOfSyllables / numOfWords) - (numOfWords / numOfSentences);
+        scoreNum = Number(scoreNum.toFixed(2));				
+		return {
+			score: scoreNum,
+			...this.getINFDifficulty(scoreNum),
+		};
+	}
+
+	// WIP
+	alMu(metrics: Metrics): muScore {
+		return {
+			score: 0,
+			difficulty: ""
+		}
+	}
+
 	// Helper services
 
 	getFHDifficulty(score: number): Pick<FernandezHuertaScore, 'difficulty' | 'schoolGrade'> {
@@ -55,5 +122,36 @@ export class AnalysisService {
 		if (score >= 80 && score < 90) return { difficulty: 'Fácil', schoolGrade: '5° grado' }; else
 		return  { difficulty: 'Muy fácil', schoolGrade: '4° grado' };
 		
-	}	
+	}
+	
+	getGPDifficulty(score: number): Pick<GutierrezPoliniScore, 'difficulty'> {
+		if (score <= 20) return { difficulty: 'Muy dificil'}; else
+		if (score >= 21 && score <= 40) return { difficulty: 'Dificil'}; else
+		if (score >= 41 && score <= 60) return { difficulty: 'Normal'}; else
+		if (score >= 61 && score <= 80) return { difficulty: 'Fácil'}; else
+		return { difficulty: 'Muy fácil'};
+		
+	}
+
+	getSPDifficulty(score: number): Pick<SzigrisztPazosScore, 'difficulty' | 'type' | 'schoolGrade'> {
+		if (score < 16) return { difficulty: 'Muy dificil', type: 'Cientifica, filosófica', schoolGrade: 'Titulados universitarios' }; else
+		if (score >= 16 && score <=35) return { difficulty: 'Árido', type: 'pedagógica, técnica', schoolGrade: 'Selectividad y estudios universitarios' }; else
+		if (score >= 36 && score <=50) return { difficulty: 'Bastante difícil', type: 'Literatura y divulgación', schoolGrade: 'Cursos secundarios' }; else
+		if (score >= 51 && score <=65) return { difficulty: 'Normal', type: 'Los media', schoolGrade: 'Popular' }; else
+		if (score >= 66 && score <=75) return { difficulty: 'Bastante fácil', type: 'Novela, revista', schoolGrade: '12 años' }; else
+		if (score >= 76 && score <=85) return { difficulty: 'Fácil', type: 'Para quioscos', schoolGrade: '11 años' }; else
+		return { difficulty: 'Muy fácil', type: 'Cómics y viñetas', schoolGrade: '6 a 10 años' };
+		
+	}
+
+	getINFDifficulty(score: number): Pick<InfleszScore, 'difficulty'> {
+		if (score < 41) return { difficulty: 'Muy dificil'}; else
+		if (score >= 41 && score <=55) return { difficulty: 'Algo dificil'}; else
+		if (score >= 56 && score <=65) return { difficulty: 'Normal'}; else
+		if (score >= 66 && score <=80) return { difficulty: 'Bastante fácil'}; else
+		return { difficulty: 'Muy fácil'};
+		
+	}
 }
+
+
