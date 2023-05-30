835e530712eab0d73ddfbf2d6e2245775b9b76a4
Jonathan Nuñez <37056901+JonathanNunezRdz@users.noreply.github.com>
2023-05-30 15:41:05 -0600

    added pm2 config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
 		node: true,
 		jest: true,
 	},
-	ignorePatterns: ['.eslintrc.js', 'checkEnv.js'],
+	ignorePatterns: ['.eslintrc.js', 'checkEnv.js', 'ecosystem.config.js'],
 	rules: {
 		'@typescript-eslint/interface-name-prefix': 'off',
 		'@typescript-eslint/explicit-function-return-type': 'off',
---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+
+const envTypes = ['dev', 'test', 'prod'];
+
+const env = {};
+
+envTypes.forEach((type) => {
+	const { parsed } = dotenv.config({ path: `./.env.${type}` });
+
+	if (parsed) {
+		env[type] = { ...parsed };
+	}
+});
+
+module.exports = {
+   apps: [
+      {
+         name: 'pef-backend',
+         script: './dist/src/main.js',
+         instances: 1,
+         env_dev: {
+            ...env['dev'],
+         },
+         env_test: {
+            ...env['test'],
+         },
+         env_prod: {
+            ...env['prod'],
+         },
+      },
+   ],
+}
