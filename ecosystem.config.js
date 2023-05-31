e8ebc5f8288389e2364d14c6dcde0f0a67dfbc5d
Jonathan Nuñez <37056901+JonathanNunezRdz@users.noreply.github.com>
2023-05-30 19:00:54 -0500

    added tsconfig for envs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,20 +13,30 @@ envTypes.forEach((type) => {
 });
 
 module.exports = {
-   apps: [
-      {
-         name: 'pef-backend',
-         script: './dist/src/main.js',
-         instances: 1,
-         env_dev: {
-            ...env['dev'],
-         },
-         env_test: {
-            ...env['test'],
-         },
-         env_prod: {
-            ...env['prod'],
-         },
-      },
-   ],
-}
+	apps: [
+		{
+			name: 'pef-backend-dev',
+			script: './dist/dev/src/main.js',
+			instances: 1,
+			env: {
+				...env['dev'],
+			},
+		},
+		{
+			name: 'pef-backend-test',
+			script: './dist/test/src/main.js',
+			instances: 1,
+			env: {
+				...env['test'],
+			},
+		},
+		{
+			name: 'pef-backend-prod',
+			script: './dist/prod/src/main.js',
+			instances: 1,
+			env: {
+				...env['prod'],
+			},
+		},
+	],
+};
